74fca0bf04fbe0b239087b5bbfbad87102797115
abishak-cit17 <56686092+abishak-cit17@users.noreply.github.com>
2021-09-03 12:25:50 +0530

    Update AdminGuestMode.java

--- src/com/itbulls/learnit/javacore/controlstatements/hw/AdminGuestMode.java
+++ src/com/itbulls/learnit/javacore/controlstatements/hw/AdminGuestMode.java
@@ -2,16 +2,22 @@ package com.itbulls.learnit.javacore.controlstatements.hw;
 
 public class AdminGuestMode {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		String inputArguments = String.join(",", args);
 		String adminFlag = "--admin";
 		String guestFlag = "--guest";
-		if (inputArguments.contains(adminFlag) && inputArguments.contains(guestFlag)) {
+		if (inputArguments.contains(adminFlag) && inputArguments.contains(guestFlag)) 
+		{
 			System.out.println("Please, select either 'ADMIN' or "
 					+ "'GUEST' mode for this program");
-		} else if (inputArguments.contains(adminFlag)) {
+		} 
+		else if (inputArguments.contains(adminFlag)) 
+		{
 			System.out.println("Hello, Admin!");
-		} else if (inputArguments.contains(guestFlag)) {
+		} 
+		else if (inputArguments.contains(guestFlag)) 
+		{
 			System.out.println("Hello, Guest!");
 		}
 		
